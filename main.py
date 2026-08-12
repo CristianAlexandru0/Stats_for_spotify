a4306c13c32f9c2ac0bd2ba1328058f751519ee2
CristianAlexandru0 <alexcristiancatalin45@gmail.com>
2026-08-12 14:52:51 +0300

    finished the recomandation algorithm

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import requests
-from config import Id, Client, Redirect
-
+from config import Id, Client, Redirect, LastFmKey
 Scope = "user-top-read user-read-private"
-
+st.set_page_config(layout="wide")
 st.sidebar.markdown(
     "<h1 style='font-size: 45px; font-weight: bold;'>Stats for Spotify</h1>", 
     unsafe_allow_html=True
@@ -81,4 +80,104 @@ else:
     first_subcol, second_subcol = second_column.columns(2)
     first_subcol.metric(label = "Spotify Subscription", value = account_sub)
     second_subcol.metric(label = "Followers", value = account_followers)
+
+    # RECOMANDATIONS    
+    from recomandation import get_genres_lastfm, get_all_genres_simultaneous, collect_genres
+    from recomandation import build_profile, transfrom_to_vector, cosinus_affinity 
+
+    artists_response = requests.get(
+                "https://api.spotify.com/v1/me/top/artists",
+                headers={"Authorization": f"Bearer {access_token}"}
+    )
+    # all user's top artists
+    top_artists = artists_response.json()["items"]
+
+    # adds more genres to the artist from last.fm
+    get_all_genres_simultaneous(top_artists, LastFmKey)
+
+    # creates a list with all the genres from the top artists
+    all_user_genres = collect_genres(top_artists)
+    # adds the genres of the top artist to build a profile
+    user_profile = build_profile(top_artists, all_user_genres)
+
+    from collections import Counter
+
+    all_top_artists_genres = []
+    # adds all the top artists genres to a list
+    for artist in top_artists:
+        all_top_artists_genres.extend(artist.get("genres",[]))
+    # returns a dictionary like variable 
+    count = Counter(all_top_artists_genres)
+
+    # gets the top 4 most listened genres in pairs like "name":"count"
+    top_4_pairs = count.most_common(4)
+
+    top_4_genres = []
+    # gets the top 4 most listened genres names
+    for pair in top_4_pairs:
+        genre_name = pair[0]
+        top_4_genres.append(genre_name)
+
+    found_artists = []
+    # extacts 10 artists from each most listened genre
+    for genre in top_4_genres: 
+        response = requests.get(
+                    "https://api.spotify.com/v1/search",
+                    headers={"Authorization": f"Bearer {access_token}"},
+                    params={"q": f'genre:"{genre}"', "type": "artist", "limit": 10}
+        )
+        data = response .json()
+        try:
+            items = data["artists"]["items"]
+        except KeyError:
+            items = []
+        found_artists.extend(items)
+
+    # adds more genres to the artist from last.fm
+    get_all_genres_simultaneous(found_artists, LastFmKey)
+
+    recomended_artist = []
+    seen_artists = []
+    top_artists_id = []
+    for artist in top_artists:
+        top_artists_id.append(artist["id"])
+
+    for artist in found_artists:
+        id = artist["id"]
+        if id not in top_artists_id and id not in seen_artists:
+            # mark the artist as already seen
+            seen_artists.append(id)
+            # transform the genres of the artist in a vector
+            vector_artist = transfrom_to_vector(artist.get("genres",[]),all_user_genres)
+            # caculate the similarity score
+            affinity_score = cosinus_affinity(vector_artist, user_profile)
+            if affinity_score > 0:
+                if artist["images"]:
+                    image_url = artist["images"][0]["url"]
+                else:
+                    image_url = "default.png"
+                # appends to a list of dictionaries the data of the artist
+                recomended_artist.append({
+                    "name": artist["name"],
+                    "image": image_url,
+                    "link": artist["external_urls"]["spotify"],
+                    "score": affinity_score
+                })
+    
+    # returns the score of an artist 
+    def extract_score(artist):
+        return artist["score"]
+    # sorts the artists by the score in descending order
+    recomended_artist.sort(key = extract_score, reverse = True)
+
+    st.markdown("Our recomandation for you:")
+    if recomended_artist:
+        recomendation_row = st.columns(10)
+        for i, recomendation in enumerate(recomended_artist[:10]):
+            recomendation_row[i].image(recomendation["image"], use_container_width = True)
+            recomendation_row[i].markdown(f"[{recomendation['name']}]({recomendation['link']})")
+            recomendation_row[i].caption(f"Similarity:{int(recomendation['score'] * 100)}%")
+    else:
+        st.info("We didn t find enough recomandations for you")
     st.success("You are connected")
+    
---
+++ recomandation.py
@@ -0,0 +1,98 @@
+import numpy as np
+import requests
+import streamlit as st
+from concurrent.futures import ThreadPoolExecutor
+
+# because executing one by one the api requests take a long time
+# this function will help with executing the requests simultaneous
+def get_all_genres_simultaneous(artists, api_key):
+    # executor is a manager that can simultaneous execute 10 functions
+    executor = ThreadPoolExecutor(max_workers=10)
+    tasks = []
+    # submit sends every artist to a thread
+    # submit doesn't wait for the function to return (it will execute in the background)
+    for artist in artists:
+        task = executor.submit(get_genres_lastfm, artist["name"], api_key)
+        tasks.append(task)\
+    # waits for all the threads to finis and syncs the result of the function (task.result) in the artist genres
+    # zip function asociates every artist with his task in order
+    for artist, task in zip(artists, tasks):
+        artist["genres"] = task.result()
+    # free the resources
+    executor.shutdown()
+
+@st.cache_data
+# requests the data of an artist from last.fm 
+# and adds the genres of the artist to a list
+def get_genres_lastfm(artist_name, api_key):
+    response = requests.get(
+         "http://ws.audioscrobbler.com/2.0/",
+        params={
+            "method": "artist.getTopTags",
+            "artist": artist_name,
+            "api_key": api_key,
+            "format": "json"
+        }
+    )
+   
+    data = response.json()
+    genres = []
+    try:
+        tags = data["toptags"]["tag"]
+        #gets the most important 3 genres
+        top_3_tags = tags[:3]
+        for tag in top_3_tags:
+            genre_name = tag["name"].lower()
+            genres.append(genre_name)
+    except KeyError:
+        return []
+    except TypeError:
+        return []
+    return genres
+    
+# collects all the genres from the user's top artists
+def collect_genres(top_artists):
+    genres = []
+    for artist in top_artists:
+        for genre in artist.get("genres",[]):
+            if genre not in genres:
+                genres.append(genre)
+    return list(genres)
+
+# the sum of all vectors of the user's top artists
+def build_profile(top_artists, all_user_genres):
+    profile = np.zeros(len(all_user_genres))
+    for artist in top_artists:
+        artist_genres = artist.get("genres", [])
+        profile = profile + transfrom_to_vector(artist_genres, all_user_genres)
+    return profile
+
+# compare the artists genres with the users genres
+# if the genres are the same it puts in a vector 1 else 0
+# with the help of numpy, it creates an array
+def transfrom_to_vector(artist_genres, user_genres):
+    vector = []
+    for genre in user_genres:
+        if genre in artist_genres:
+            vector.append(1)
+        else:
+            vector.append(0)
+    return np.array(vector)
+
+ # calculates the likness of the two vectors to be similar
+def cosinus_affinity(vector1, vector2):
+    # calculates the norm of the two vectors
+    norm_vector1 = np.linalg.norm(vector1)
+    norm_vector2 = np.linalg.norm(vector2)
+
+    if norm_vector1 == 0 or norm_vector2 == 0:
+        return 0
+    
+    # calculates the dot product of the vectors
+    dot_product = np.dot(vector1, vector2)
+
+    affinity = dot_product / (norm_vector1 * norm_vector2)
+    return(affinity)
+
+    
+    
